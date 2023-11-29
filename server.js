78aaa452739772083baa85679a3963de011acd08
Marko Radulovic (RIT Student) <mr7287@dbv-rm24-2301.main.ad.rit.edu>
2023-11-29 08:21:15 +0100

    minor changes

--- server.js
+++ server.js
@@ -1,25 +1,16 @@
 const express = require("express")
 const app = express()
 const port = 3000
-const { getStocksData,getStockData } = require("./stock");
+const { getStocksData, getStockData } = require("./stock");
 let stocks = {}
 const nasdaqStocks = [
-    "AAPL", "AMZN", "MSFT", "GOOGL", "META", "NVDA", "INTC", "CSCO", "ADBE","NFLX",
-    "SBUX", "GILD", "PYPL", "AMGN", "BIIB", "AMAT","REGN", "CMCSA","MU", "ILMN", 
-    "KHC","AMD","COST","PEP","QCOM","TXN","HON","ADI","VRTX","PANW" ];
+    "AAPL", "AMZN", "MSFT", "GOOGL", "META", "NVDA", "INTC", "CSCO", "ADBE", "NFLX",
+    "SBUX", "GILD", "PYPL", "AMGN", "BIIB", "AMAT", "REGN", "CMCSA", "MU", "ILMN",
+    "KHC", "AMD", "COST", "PEP", "QCOM", "TXN", "HON", "ADI", "VRTX", "PANW"];
 
-app.get('/', async(req, res) => {
+app.get('/', async (req, res) => {
     res.send('Hello World!')
-     stocks = await getStocksData(nasdaqStocks)
-        .then((stocksData) => {
-            return stocksData
-        })
-        .catch((error) => {
-            console.error('Error fetching stock data:', error.message);
-        });
-        console.log("Stocks are done loading")
-
-    }
+}
 );
 
 
@@ -32,11 +23,11 @@ app.listen(port, async () => {
         .catch((error) => {
             console.error('Error fetching stock data:', error.message);
         });
-        console.log("Stocks are done loading")
+    console.log("Stocks are done loading")
 
-    }
+}
 );
 
-app.get('/api/getStocks', async(req, res) => {
+app.get('/api/getStocks', async (req, res) => {
     res.json(stocks)
 });
