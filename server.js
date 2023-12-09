9489ccd7385f47dabfffaa70a238c70030cc776e
Marko Radulovic (RIT Student) <mr7287@dbv-rm24-2301.main.ad.rit.edu>
2023-12-09 14:47:49 +0100

    updated the api

--- server.js
+++ server.js
@@ -1,20 +1,17 @@
 const express = require("express")
 const app = express()
 const port = 3000
-const { getStocksData } = require("./stock");
+const { getStocksData, getStockData } = require("./stock");
 let stocks = {}
 const nasdaqStocks = [
     "AAPL", "AMZN", "MSFT", "GOOGL", "META", "NVDA", "INTC", "CSCO", "ADBE", "NFLX",
     "SBUX", "GILD", "PYPL", "AMGN", "BIIB", "AMAT", "REGN", "CMCSA", "MU", "ILMN",
     "KHC", "AMD", "COST", "PEP", "QCOM", "TXN", "HON", "ADI", "VRTX", "PANW"];
 
-
-
-
 async function fetchStocksData() {
     try {
         stocks = await getStocksData(nasdaqStocks);
-        console.log('Stocks are done loading:');
+        console.log('Stocks are done loading');
     } catch (error) {
         console.error('Error fetching stock data:', error.message);
     }
@@ -44,3 +41,9 @@ app.listen(port, async () => {
 app.get('/api/getStocks', async (req, res) => {
     res.json(stocks)
 });
+
+
+app.get('/api/getUpdatedStock/:symbol', async (req, res) => {
+    const stock = await getStockData(req.params.symbol)
+    res.json(stock)
+})
